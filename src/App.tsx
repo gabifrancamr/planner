c98dab700e9b08ff8fb78b98851b8c12cc203f9d
Gabriela <gabifmouraroc@outlook.com>
2024-07-09 17:41:48 -0300

    mostrando quantidade de pessoas convidadas

--- src/App.tsx
+++ src/App.tsx
@@ -129,12 +129,21 @@ export function App() {
                 className="flex items-center gap-2 flex-1 text-left"
               >
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <span className="text-lg text-zinc-400 flex-1">
-                  Quem estará na viagem?
-                </span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-lg text-zinc-100 flex-1">
+                    {emailsToInvite.length} pessoa(s) convidada(s)
+                  </span>
+                ) : (
+                  <span className="text-lg text-zinc-400 flex-1">
+                    Quem estará na viagem?
+                  </span>
+                )}
               </button>
 
-              <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <button
+                onClick={openConfirmTripModal}
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
                 Confirmar viagem
                 <ArrowRight className="size-5" />
               </button>
