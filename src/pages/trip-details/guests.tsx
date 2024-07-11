f0c70e45f7c3ae65592474ad25927493d4ac58e9
Gabriela <gabifmouraroc@outlook.com>
2024-07-11 16:38:54 -0300

    adicionando novos convidados

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { api } from "../../lib/axios";
+import { InviteModal } from "./invite-modal";
 
 interface Participant {
   id: string;
@@ -14,6 +15,7 @@ interface Participant {
 export function Guests() {
   const { tripId } = useParams();
   const [participants, setParticipants] = useState<Participant[]>([]);
+  const [isInviteModalOpen, setIsInviteModalOpen] = useState(false);
 
   useEffect(() => {
     api
@@ -21,13 +23,23 @@ export function Guests() {
       .then((response) => setParticipants(response.data.participants));
   }, [tripId]);
 
+  function openInviteModal() {
+    setIsInviteModalOpen(true);
+  }
+
+  function closeInviteModal() {
+    setIsInviteModalOpen(false);
+  }
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
 
       <div className="space-y-5">
         {participants.map((participant, index) => (
-          <div key={participant.id} className="flex items-center justify-between gap-4">
+          <div
+            key={participant.id}
+            className="flex items-center justify-between gap-4"
+          >
             <div className="space-y-1.5 flex-1">
               <span className="block font-medium text-zinc-100">
                 {participant.name ?? `Convidado ${index}`}
@@ -39,16 +51,18 @@ export function Guests() {
             {participant.is_confirmed ? (
               <CheckCircle2 className="text-green-400 size-5 shrink-0" />
             ) : (
-              <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              <CircleDashed className="text-zinc-400 size-5 shrink-0 cursor-pointer" />
             )}
           </div>
         ))}
       </div>
 
-      <Button variant="secondary" size="full">
+      <Button onClick={openInviteModal} variant="secondary" size="full">
         <UserCog className="size-5" />
         Gerenciar convidados
       </Button>
+
+      {isInviteModalOpen && <InviteModal closeInviteModal={closeInviteModal} />}
     </div>
   );
 }
