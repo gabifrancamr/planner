3b24231a7650f99866124067062e156124a59f54
Gabriela <gabifmouraroc@outlook.com>
2024-07-11 12:09:38 -0300

    criando viagem usando API

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,7 @@ import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-date-step";
 import { InviteGuestsStep } from "./steps/invite-guests-step";
 import { DateRange } from "react-day-picker";
+import { api } from "../../lib/axios";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -14,9 +15,9 @@ export function CreateTripPage() {
   const [emailsToInvite, setEmailsToInvite] = useState(["gabi@email.com"]);
   const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
 
-  const [destination, setDestination] = useState("")
-  const [ownerName, setOwnerName] = useState("")
-  const [ownerEmail, setOwnerEmail] = useState("")
+  const [destination, setDestination] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [ownerEmail, setOwnerEmail] = useState("");
   const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
     DateRange | undefined
   >();
@@ -72,20 +73,43 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList);
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log(destination)
+    console.log(destination);
+    console.log(ownerName);
+    console.log(ownerEmail);
+    console.log(emailsToInvite);
+    console.log(eventStartAndEndDates);
 
-    console.log(ownerName)
+    if(!destination) {
+      return
+    }
 
-    console.log(ownerEmail)
+    if(!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) {
+      return
+    }
 
-    console.log(emailsToInvite)
+    if(emailsToInvite.length === 0) {
+      return
+    }
 
-    console.log(eventStartAndEndDates)
+    if (!ownerName || !ownerEmail) {
+      return
+    }
 
-    //navigate("/trips/123");
+    const response = await api.post("/trips", {
+      destination,
+      starts_at: eventStartAndEndDates?.from,
+      ends_at: eventStartAndEndDates?.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail,
+    });
+
+    const { tripId } = response.data
+
+    navigate(`/trips/${tripId}`);
   }
 
   return (
